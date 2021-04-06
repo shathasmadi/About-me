4240bb1b7682ed1e3fd304447f2cf581841d26f1
shathasmadi <alsmadishatha@gmail.com>
2021-04-06 11:29:08 +0300

    edit and push

--- javascript/main.js
+++ javascript/main.js
@@ -7,9 +7,13 @@ let q1 = prompt('are you here')
 q1 = q1.toUpperCase();
 switch(q1){
     case('YES'):
+    case('Y'):
+    alert('Iam here');
     alert('Iam here');
     break;
     case('NO'):
+    case('N'):
+    alert('Iam in the home');
     alert('Iam in the home');
     break;
     default:
@@ -21,9 +25,13 @@ let q2 = prompt('your age is greater than 22? ')
 q2 = q2.toLowerCase();
 switch(q2){
     case('yes'):
+    case('y'):
+    alert('your age is allowed in this page');
     alert('your age is allowed in this page');
     break;
     case('no'):
+    case('n'):
+    alert('ok no problem ');
     alert('ok no problem ');
     break;
     default:
@@ -35,9 +43,13 @@ let q3 = prompt('are you live in Amman? ')
 q3 = q3.toLowerCase();
 switch(q3){
     case('yes'):
+    case('y'):
+    alert('your live in Amman');
     alert('your live in Amman');
     break;
     case('no'):
+    case('n'):
+    alert('your live out Amman ');
     alert('your live out Amman ');
     break;
     default:
@@ -49,9 +61,13 @@ let q4 = prompt('are you love makeup?')
 q4 = q4.toLowerCase();
 switch(q4){
     case('yes'):
+    case('y'):
+    alert('you love makeup');
     alert('you love makeup');
     break;
     case('no'):
+    case('n'):
+    alert('you  dont love makeup');
     alert('you  dont love makeup');
     break;
     default:
@@ -63,9 +79,13 @@ let q5 = prompt('do yo think i love makeup?')
 q5 = q5.toLowerCase();
 switch(q5){
     case('yes'):
+    case('y'):
+    alert('you answer is true');
     alert('you answer is true');
     break;
     case('no'):
+    case('n'):
+    alert('you answer is false');
     alert('you answer is false');
     break;
     default:
