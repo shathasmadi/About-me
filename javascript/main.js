fad419d6363348b5d316f9ac5f358e9f3fe97a04
Abdullah Alabbadi <abdullah_alabbadi2018@yahoo.com>
2021-04-07 15:51:45 +0300

    add function for seventh question

--- javascript/main.js
+++ javascript/main.js
@@ -52,114 +52,116 @@ question2();
 
 function question3() {
 
-let q3 = prompt('are you live in Amman? ')
-//console.log(q3);
-q3 = q3.toLowerCase();
-switch (q3) {
-    case ('yes'):
-    case ('y'):
-        score++;
-        alert('your live in Amman');
-        break;
-    case ('no'):
-    case ('n'):
-        alert('your live out Amman ');
-        break;
-    default:
-        alert('welcome');
-}
+    let q3 = prompt('are you live in Amman? ')
+    //console.log(q3);
+    q3 = q3.toLowerCase();
+    switch (q3) {
+        case ('yes'):
+        case ('y'):
+            score++;
+            alert('your live in Amman');
+            break;
+        case ('no'):
+        case ('n'):
+            alert('your live out Amman ');
+            break;
+        default:
+            alert('welcome');
+    }
 }
 question3();
 
 function question4() {
-let q4 = prompt('are you love makeup?')
-//console.log(q4);
-q4 = q4.toLowerCase();
-switch (q4) {
-    case ('yes'):
-    case ('y'):
-        score++;
-        alert('you love makeup');
-        break;
-    case ('no'):
-    case ('n'):
-        alert('you  dont love makeup');
-        break;
-    default:
-        alert('ok no problem');
-}
+    let q4 = prompt('are you love makeup?')
+    //console.log(q4);
+    q4 = q4.toLowerCase();
+    switch (q4) {
+        case ('yes'):
+        case ('y'):
+            score++;
+            alert('you love makeup');
+            break;
+        case ('no'):
+        case ('n'):
+            alert('you  dont love makeup');
+            break;
+        default:
+            alert('ok no problem');
+    }
 }
 question4();
 
 function question5() {
-let q5 = prompt('do yo think i love makeup?')
-//console.log(q5);
-q5 = q5.toLowerCase();
-switch (q5) {
-    case ('yes'):
-    case ('y'):
-        score++;
-        alert('you answer is true');
-        break;
-    case ('no'):
-    case ('n'):
-        alert('you answer is false');
-        break;
-    default:
-        alert('ok no problem');
-}
+    let q5 = prompt('do yo think i love makeup?')
+    //console.log(q5);
+    q5 = q5.toLowerCase();
+    switch (q5) {
+        case ('yes'):
+        case ('y'):
+            score++;
+            alert('you answer is true');
+            break;
+        case ('no'):
+        case ('n'):
+            alert('you answer is false');
+            break;
+        default:
+            alert('ok no problem');
+    }
 }
 question5();
 
 function question6() {
-let guessNum = 0;
-for (let i = 1; i <= 4; i++) {
-    guessNum = parseInt(prompt('Guess a number between 1-10 I\'ll tell you what\'s my number'));
-    if (guessNum === 5) {
-        alert('you gussed');
-        break;
-        score++;
-    } else if (guessNum > 5) {
+    let guessNum = 0;
+    for (let i = 1; i <= 4; i++) {
+        guessNum = parseInt(prompt('Guess a number between 1-10 I\'ll tell you what\'s my number'));
+        if (guessNum === 5) {
+            alert('you gussed');
+            break;
+            score++;
+        } else if (guessNum > 5) {
 
-        //alert('Too high'+ 'Try again');
-        alert('To high try again');
+            //alert('Too high'+ 'Try again');
+            alert('To high try again');
 
 
-    } else {
+        } else {
 
-        //alert('Too low'+ 'Try again');
-        alert('To low try again');
+            //alert('Too low'+ 'Try again');
+            alert('To low try again');
 
+        }
+        if (i === 4) {
+            alert('Thank you to try the number is 5')
+            break;
+        }
     }
-    if (i === 4) {
-        alert('Thank you to try the number is 5')
-        break;
-    }
-}
 }
 question6();
 
+function question7() {
 
-let guessFav = ['piano', 'shopping'];
-console.log(guessFav);
+    let guessFav = ['piano', 'shopping'];
+    console.log(guessFav);
 
-for (let i = 1; i <= 6; i++) {
-    let favInput = prompt('guessing what\'s my favourite thing its 2 guessing at least 1')
-    favInput = favInput.toLowerCase();
-    if (favInput === guessFav[0] || favInput === guessFav[1]) {
-        alert('yes right you are agreat');
-        score++;
-        break;
+    for (let i = 1; i <= 6; i++) {
+        let favInput = prompt('guessing what\'s my favourite thing its 2 guessing at least 1')
+        favInput = favInput.toLowerCase();
+        if (favInput === guessFav[0] || favInput === guessFav[1]) {
+            alert('yes right you are agreat');
+            score++;
+            break;
+        }
+
+        else if (i === 6) {
+            alert('my favourite thing is ' + guessFav[0]);
+
+            alert('my favourite thing is ' + guessFav[1]);
+        }
+
     }
-
-    else if (i === 6) {
-        alert('my favourite thing is ' + guessFav[0]);
-
-        alert('my favourite thing is ' + guessFav[1]);
-    }
+    alert('welcom ' + userName + 'to visit my website your score is ' + score + '/7');
 
 }
-alert('welcom ' + userName + 'to visit my website your score is ' + score + '/7');
-
-
+question7();
 
